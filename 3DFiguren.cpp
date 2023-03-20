511a2a49f8fdca19f3fb428a9a37c40adac15884
GhyselsT <thomghysels@gmail.com>
2023-03-20 16:03:05 +0100

    Sphere 3Dfigs

--- 3DFiguren.cpp
+++ 3DFiguren.cpp
@@ -83,14 +83,14 @@ Figure createIcosahedron(){
             i.points.emplace_back(Vector3D::point(0, 0, sqrt(5)/2));
         }
         else if (teller == 2 or teller == 3 or teller == 4 or teller == 5 or teller == 6){
-            double x = cos((teller-2)*2*M_PI/5);
-            double y = sin((teller-2)*2*M_PI/5);
+            double x = cos((teller - 2) * 2 * M_PI/5);
+            double y = sin((teller - 2) * 2 * M_PI/5);
             double z = 0.5;
             i.points.emplace_back(Vector3D::point(x, y, z));
         }
         else if (teller == 7 or teller == 8 or teller == 9 or teller == 10 or teller == 11){
-            double x = cos(M_PI/5 + (teller-7)*2*M_PI/5);
-            double y = sin(M_PI/5 + (teller-7)*2*M_PI/5);
+            double x = cos(M_PI/5 + (teller - 7) * 2 * M_PI/5);
+            double y = sin(M_PI/5 + (teller - 7) * 2 * M_PI/5);
             double z = -0.5;
             i.points.emplace_back(Vector3D::point(x, y, z));
         }
@@ -122,4 +122,54 @@ Figure createIcosahedron(){
 
     return i;
 }
+Vector3D GetMidTriangle(vector<Vector3D> points){
+    double x = 0.0, y = 0.0 , z = 0.0;
+    for (int i = 0; i < 3; ++i) {
+        x += points[i].x;
+        y += points[i].y;
+        z += points[i].z;
+    }
+    x/=3, y/=3, z/=3;
+    return Vector3D::point(x,y,z);
+}
+
+Figure createDodecahedron(){
+    Figure d;
+    //points emplacen
+    Figure ico = createIcosahedron();
+    for(const auto& face:ico.faces ){
+        vector<Vector3D> p;
+        for(auto item : face.point_indexes){
+            p.emplace_back(ico.points[item]);
+        }
+        d.points.emplace_back(GetMidTriangle(p));
+    }
+
+    //Faces emplacen
+    d.faces.emplace_back(Face({0,1,2,3,4}));
+    d.faces.emplace_back(Face({0,5,6,7,1}));
+    d.faces.emplace_back(Face({1,7,8,9,2}));
+    d.faces.emplace_back(Face({2,9,10,11,3}));
+    d.faces.emplace_back(Face({3,11,12,13,4}));
+    d.faces.emplace_back(Face({4,13,14,5,0}));
+    d.faces.emplace_back(Face({19,18,17,16,15}));
+    d.faces.emplace_back(Face({19,14,13,12,18}));
+    d.faces.emplace_back(Face({18,12,11,10,17}));
+    d.faces.emplace_back(Face({17,10,9,8,6}));
+    d.faces.emplace_back(Face({16,8,7,6,15}));
+    d.faces.emplace_back(Face({15,6,5,14,19}));
+
+    return d;
+}
+
+Figure createSphere(const double radius, const int n){
+    Figure sphere;
+
+    Figure ico = createIcosahedron();
+    for (int i = 0; i < n; ++i) {
+
+    }
+    return sphere;
+}
+
 
