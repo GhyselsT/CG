89b72da4b37e53af9a9cbeecc56beeef2fb7969a
GhyselsT <thomghysels@gmail.com>
2023-05-10 16:38:13 +0200

    read

--- cmake-build-debug/CMakeFiles/engine.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/engine.dir/DependInfo.cmake
@@ -8,14 +8,14 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/student/CLionProjects/CG/Functies.cpp" "CMakeFiles/engine.dir/Functies.cpp.o" "gcc" "CMakeFiles/engine.dir/Functies.cpp.o.d"
-  "/home/student/CLionProjects/CG/ZBuffer.cpp" "CMakeFiles/engine.dir/ZBuffer.cpp.o" "gcc" "CMakeFiles/engine.dir/ZBuffer.cpp.o.d"
-  "/home/student/CLionProjects/CG/draw2DLines.cpp" "CMakeFiles/engine.dir/draw2DLines.cpp.o" "gcc" "CMakeFiles/engine.dir/draw2DLines.cpp.o.d"
-  "/home/student/CLionProjects/CG/easy_image.cc" "CMakeFiles/engine.dir/easy_image.cc.o" "gcc" "CMakeFiles/engine.dir/easy_image.cc.o.d"
-  "/home/student/CLionProjects/CG/engine.cc" "CMakeFiles/engine.dir/engine.cc.o" "gcc" "CMakeFiles/engine.dir/engine.cc.o.d"
-  "/home/student/CLionProjects/CG/ini_configuration.cc" "CMakeFiles/engine.dir/ini_configuration.cc.o" "gcc" "CMakeFiles/engine.dir/ini_configuration.cc.o.d"
-  "/home/student/CLionProjects/CG/l_parser.cc" "CMakeFiles/engine.dir/l_parser.cc.o" "gcc" "CMakeFiles/engine.dir/l_parser.cc.o.d"
-  "/home/student/CLionProjects/CG/vector3d.cc" "CMakeFiles/engine.dir/vector3d.cc.o" "gcc" "CMakeFiles/engine.dir/vector3d.cc.o.d"
+  "C:/Users/thomg/Documents/SCHOOL/sem2/CG/Functies.cpp" "CMakeFiles/engine.dir/Functies.cpp.obj" "gcc" "CMakeFiles/engine.dir/Functies.cpp.obj.d"
+  "C:/Users/thomg/Documents/SCHOOL/sem2/CG/ZBuffer.cpp" "CMakeFiles/engine.dir/ZBuffer.cpp.obj" "gcc" "CMakeFiles/engine.dir/ZBuffer.cpp.obj.d"
+  "C:/Users/thomg/Documents/SCHOOL/sem2/CG/draw2DLines.cpp" "CMakeFiles/engine.dir/draw2DLines.cpp.obj" "gcc" "CMakeFiles/engine.dir/draw2DLines.cpp.obj.d"
+  "C:/Users/thomg/Documents/SCHOOL/sem2/CG/easy_image.cc" "CMakeFiles/engine.dir/easy_image.cc.obj" "gcc" "CMakeFiles/engine.dir/easy_image.cc.obj.d"
+  "C:/Users/thomg/Documents/SCHOOL/sem2/CG/engine.cc" "CMakeFiles/engine.dir/engine.cc.obj" "gcc" "CMakeFiles/engine.dir/engine.cc.obj.d"
+  "C:/Users/thomg/Documents/SCHOOL/sem2/CG/ini_configuration.cc" "CMakeFiles/engine.dir/ini_configuration.cc.obj" "gcc" "CMakeFiles/engine.dir/ini_configuration.cc.obj.d"
+  "C:/Users/thomg/Documents/SCHOOL/sem2/CG/l_parser.cc" "CMakeFiles/engine.dir/l_parser.cc.obj" "gcc" "CMakeFiles/engine.dir/l_parser.cc.obj.d"
+  "C:/Users/thomg/Documents/SCHOOL/sem2/CG/vector3d.cc" "CMakeFiles/engine.dir/vector3d.cc.obj" "gcc" "CMakeFiles/engine.dir/vector3d.cc.obj.d"
   )
 
 # Targets to which this target links.
